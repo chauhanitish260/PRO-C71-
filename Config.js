3e4deab879c1d3838254ec72e1baf888f08db195
chauhanitish260 <76865515+chauhanitish260@users.noreply.github.com>
2021-07-16 16:14:54 +0530

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+require('@firebase/firestore')
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBET5rFxvH0fP4NqKOuBW5f4UXYnu5HhM0",
+    authDomain: "pro-c71-story-hub---2-25d54.firebaseapp.com",
+    projectId: "pro-c71-story-hub---2-25d54",
+    storageBucket: "pro-c71-story-hub---2-25d54.appspot.com",
+    messagingSenderId: "125885095145",
+    appId: "1:125885095145:web:98dc153300ede437506478"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
